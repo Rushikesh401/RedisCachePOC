204c6c3559a1dbe6e309470b4d22529ea97013c3
Rushikesh Suradkar <rushikeshsuradkar2000@gmail.com>
2024-03-14 11:27:16 -0700

    Initial commit

---
+++ Data/DbContextClass.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedisPOC.Models;
+
+namespace RedisPOC.Data
+{
+    public class DbContextClass : DbContext
+    {
+        public DbContextClass(DbContextOptions<DbContextClass> dbContextOptions) : base(dbContextOptions) 
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
